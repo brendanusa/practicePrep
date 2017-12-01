ea4dcfd9724031c5a89b16dd427cbc1df193d263
Brendan Bansavage <brendaniscool@gmail.com>
2017-11-30 16:08:24 -0800

    getting there

--- nQueens.js
+++ nQueens.js
@@ -1,6 +1,7 @@
 class Board {
 
   constructor(n) {
+    this.n = n;
     this.matrix = new Array(n).fill([]).map(row => new Array(n).fill('.'));
   }
 
@@ -15,13 +16,13 @@ class Board {
 
     let k = 0
 
-    while (k < n) {
+    while (k < this.n) {
       availableRows[k] = 1;
       availableColumns[k] = 1;
       availableDiagRight[k] = 1;
       availableDiagLeft[k] = 1;
-      availableDiagRight[n * 2 - 1 - k] = 1;
-      availableDiagLeft[n * 2 - 1 - k] = 1;
+      availableDiagRight[this.n * 2 - 1 - k] = 1;
+      availableDiagLeft[this.n * 2 - 1 - k] = 1;
       k++;
     }
 
@@ -31,70 +32,127 @@ class Board {
     let startColumn = 0;
 
     var checkOneCell = (x, y) => {
-      if (availableRows[x] && availableColumns[y] && availableDiagRight[n - 1 - x + y] && availableDiagLeft[x + y]) {
+      if (availableRows[x] && availableColumns[y] && availableDiagRight[this.n - 1 - x + y] && availableDiagLeft[x + y]) {
         return true;
       }
       return false;
     }
 
-    var removeQueen = (x, y) => {
-      queens.pop();
+    var removeLastQueen = () => {
+      let x = queens[queens.length - 1][0];
+      let y = queens[queens.length - 1][1];
       this.matrix[x][y] = '.';
       availableRows[x] = 1;
       availableColumns[y] = 1;
-      availableDiagRight[n - 1 - x + y] = 1;
+      availableDiagRight[this.n - 1 - x + y] = 1;
       availableDiagLeft[x + y] = 1;
+      queens.pop();
     }
 
-    var restart = () => {
-      queens.forEach(queen => removeQueen(queen[0], queen[1]));
-      placeQueen(0, startColumn)
-    }
+    // var restart = () => {
+    //   while (queens.length > 0) {
+    //     removeLastQueen();
+    //   }
+    //   placeQueen(0, startColumn);
+    // }
 
     var checkEachCell = (x, y) => {
-      if (x < n) {
-        while (y < n) {
+
+      var iterateLastQueen = () => {
+        if (queens.length > 0) {
+          x = queens[queens.length - 1][0];
+          y = queens[queens.length - 1][1] + 1;
+          removeLastQueen();
+          // if previous queen was in last cell in row...
+          if (y === this.n) {
+            iterateLastQueen();
+          } else {
+            checkEachCell(x, y);
+          }
+        } else {
+          startColumn++;
+          if (startColumn >= this.n) {
+            return;
+          }
+          placeQueen(0, startColumn);
+        }
+      }
+
+      if (x < this.n) {
+        // in this row
+        while (y < this.n) {
+          // check this cell
           if (checkOneCell(x, y)) {
+            // if available, place queen at this cell
             return placeQueen(x, y);
           }
+          // move to next cell in row
           y++;
         }
-        checkEachCell(x + 1, 0);
+        // if no queens placed in x row, move previous queen to next cell
+        iterateLastQueen();
+        // checkEachCell(x + 1, 0);
       } else {
-        let lastQueen = queens[queens.length - 1];
-        removeQueen(lastQueen[0], lastQueen[1]);
-        checkEachCell(lastQueen[0], lastQueen[1] + 1);
+        if (queens.length > 0) {
+          iterateLastQueen();
+        } else {
+          startColumn++;
+          if (startColumn >= this.n) {
+            return;
+          }
+          placeQueen(0, startColumn);
+        }
       }
     }
 
     var placeQueen = (x, y) => {
+      // update board
       this.matrix[x][y] = 'Q';
+
+      // update queens list
       queens.push([x, y]);
 
-      if (queens.length === n) {
-        solutions.push(this.matrix.toString());
-        startColumn++;
-        if (startColumn < n) {
-          restart();
-        } else {
-          return;
-        }
-      }
-
+      // update available cells
       delete availableRows[x];
       delete availableColumns[y];
-      delete availableDiagRight[n - 1 - x + y];
+      delete availableDiagRight[this.n - 1 - x + y];
       delete availableDiagLeft[x + y];
 
-      checkEachCell(x + 1);
+      // if this is the nth queen on the board, add board to solutions
+      if (queens.length === this.n) {
+        solutions.push(this.matrix);
+      }
+
+      // if queen was placed in the bottom row...
+      if (x === this.n - 1) {
+        console.log(y)
+        // console.log(board.matrix)
+        console.log(solutions)
+        removeLastQueen();
+        x = queens[queens.length - 1][0];
+        y = queens[queens.length - 1][1] + 1;
+        removeLastQueen();
+
+        // if previous queen was in last cell in row...
+        if (y === this.n) {
+          x = queens[queens.length - 1][0];
+          y = queens[queens.length - 1][1] + 1;
+          removeLastQueen();
+        }
+        checkEachCell(x, y);
+      }
+
+      // move to next row
+      checkEachCell(x + 1, 0);
     }
 
     placeQueen(0, 0);
+
+    return solutions;
 
   }
 
 }
 
-var board = new Board(2);
-
-console.log(board.matrix)
+var board = new Board(4);
+console.log(board.solve());
